3511471689a18304e6c6cf354ab29ff3d28f5eac
rapPayne <bob@creator.net>
2016-11-12 17:27:16 -0600

    Changed JSON data to lowercase. More JavaScripty

--- Starters/Lab 2 Debugging/surveyResults.js
+++ Starters/Lab 2 Debugging/surveyResults.js
@@ -1,242 +1,242 @@
 var raw_survey_results = [
   {
     "Day": 1,
-    "Excellent": 20,
-    "Good": 25,
-    "Okay": 35,
-    "Poor": 15,
-    "Terrible": 5
+    "excellent": 20,
+    "good": 25,
+    "okay": 35,
+    "poor": 15,
+    "terrible": 5
   },
   {
     "Day": 2,
-    "Excellent": 22,
-    "Good": 22,
-    "Okay": 36,
-    "Poor": 16,
-    "Terrible": 4
+    "excellent": 22,
+    "good": 22,
+    "okay": 36,
+    "poor": 16,
+    "terrible": 4
   },
   {
     "Day": 3,
-    "Excellent": 24,
-    "Good": 26,
-    "Okay": 35,
-    "Poor": 15,
-    "Terrible": 5
+    "excellent": 24,
+    "good": 26,
+    "okay": 35,
+    "poor": 15,
+    "terrible": 5
   },
   {
     "Day": 4,
-    "Excellent": 21,
-    "Good": 27,
-    "Okay": 34,
-    "Poor": 14,
-    "Terrible": 4
+    "excellent": 21,
+    "good": 27,
+    "okay": 34,
+    "poor": 14,
+    "terrible": 4
   },
   {
     "Day": 5,
-    "Excellent": 23,
-    "Good": 27,
-    "Okay": 32,
-    "Poor": 15,
-    "Terrible": 3
+    "excellent": 23,
+    "good": 27,
+    "okay": 32,
+    "poor": 15,
+    "terrible": 3
   },
   {
     "Day": 6,
-    "Excellent": 24,
-    "Good": 30,
-    "Okay": 29,
-    "Poor": 13,
-    "Terrible": 4
+    "excellent": 24,
+    "good": 30,
+    "okay": 29,
+    "poor": 13,
+    "terrible": 4
   },
   {
     "Day": 7,
-    "Excellent": 25,
-    "Good": 30,
-    "Okay": 28,
-    "Poor": 13,
-    "Terrible": 4
+    "excellent": 25,
+    "good": 30,
+    "okay": 28,
+    "poor": 13,
+    "terrible": 4
   },
   {
     "Day": 8,
-    "Excellent": 23,
-    "Good": 34,
-    "Okay": 26,
-    "Poor": 12,
-    "Terrible": 3
+    "excellent": 23,
+    "good": 34,
+    "okay": 26,
+    "poor": 12,
+    "terrible": 3
   },
   {
     "Day": 9,
-    "Excellent": 22,
-    "Good": 36,
-    "Okay": 27,
-    "Poor": 12,
-    "Terrible": 1
+    "excellent": 22,
+    "good": 36,
+    "okay": 27,
+    "poor": 12,
+    "terrible": 1
   },
   {
     "Day": 10,
-    "Excellent": 23,
-    "Good": 33,
-    "Okay": 28,
-    "Poor": 14,
-    "Terrible": 2
+    "excellent": 23,
+    "good": 33,
+    "okay": 28,
+    "poor": 14,
+    "terrible": 2
   },
   {
     "Day": 11,
-    "Excellent": 22,
-    "Good": 36,
-    "Okay": 26,
-    "Poor": 13,
-    "Terrible": 3
+    "excellent": 22,
+    "good": 36,
+    "okay": 26,
+    "poor": 13,
+    "terrible": 3
   },
   {
     "Day": 12,
-    "Excellent": 24,
-    "Good": 38,
-    "Okay": 25,
-    "Poor": 11,
-    "Terrible": 2
+    "excellent": 24,
+    "good": 38,
+    "okay": 25,
+    "poor": 11,
+    "terrible": 2
         },
   {
     "Day": 13,
-    "Excellent": 25,
-    "Good": 39,
-    "Okay": 23,
-    "Poor": 10,
-    "Terrible": 3
+    "excellent": 25,
+    "good": 39,
+    "okay": 23,
+    "poor": 10,
+    "terrible": 3
         },
   {
     "Day": 14,
-    "Excellent": 27,
-    "Good": 38,
-    "Okay": 23,
-    "Poor": 11,
-    "Terrible": 2
+    "excellent": 27,
+    "good": 38,
+    "okay": 23,
+    "poor": 11,
+    "terrible": 2
         },
   {
     "Day": 15,
-    "Excellent": 24,
-    "Good": 32,
-    "Okay": 22,
-    "Poor": 10,
-    "Terrible": 2
+    "excellent": 24,
+    "good": 32,
+    "okay": 22,
+    "poor": 10,
+    "terrible": 2
         },
   {
     "Day": 16,
-    "Excellent": 27,
-    "Good": 32,
-    "Okay": 20,
-    "Poor": 10,
-    "Terrible": 1
+    "excellent": 27,
+    "good": 32,
+    "okay": 20,
+    "poor": 10,
+    "terrible": 1
         },
   {
     "Day": 17,
-    "Excellent": 30,
-    "Good": 39,
-    "Okay": 21,
-    "Poor": 9,
-    "Terrible": 1
+    "excellent": 30,
+    "good": 39,
+    "okay": 21,
+    "poor": 9,
+    "terrible": 1
         },
   {
     "Day": 18,
-    "Excellent": 29,
-    "Good": 43,
-    "Okay": 19,
-    "Poor": 8,
-    "Terrible": 1
+    "excellent": 29,
+    "good": 43,
+    "okay": 19,
+    "poor": 8,
+    "terrible": 1
         },
   {
     "Day": 19,
-    "Excellent": 32,
-    "Good": 42,
-    "Okay": 18,
-    "Poor": 8,
-    "Terrible": 0
+    "excellent": 32,
+    "good": 42,
+    "okay": 18,
+    "poor": 8,
+    "terrible": 0
   },
   {
     "Day": 20,
-    "Excellent": 33,
-    "Good": 41,
-    "Okay": 19,
-    "Poor": 7,
-    "Terrible": 0
+    "excellent": 33,
+    "good": 41,
+    "okay": 19,
+    "poor": 7,
+    "terrible": 0
   },
   {
     "Day": 21,
-    "Excellent": 35,
-    "Good": 39,
-    "Okay": 17,
-    "Poor": 8,
-    "Terrible": 1
+    "excellent": 35,
+    "good": 39,
+    "okay": 17,
+    "poor": 8,
+    "terrible": 1
   },
   {
     "Day": 22,
-    "Excellent": 37,
-    "Good": 41,
-    "Okay": 16,
-    "Poor": 6,
-    "Terrible": 0
+    "excellent": 37,
+    "good": 41,
+    "okay": 16,
+    "poor": 6,
+    "terrible": 0
   },
   {
     "Day": 23,
-    "Excellent": 39,
-    "Good": 39,
-    "Okay": 16,
-    "Poor": 6,
-    "Terrible": 0
+    "excellent": 39,
+    "good": 39,
+    "okay": 16,
+    "poor": 6,
+    "terrible": 0
   },
   {
     "Day": 24,
-    "Excellent": 38,
-    "Good": 42,
-    "Okay": 15,
-    "Poor": 5,
-    "Terrible": 0
+    "excellent": 38,
+    "good": 42,
+    "okay": 15,
+    "poor": 5,
+    "terrible": 0
   },
   {
     "Day": 25,
-    "Excellent": 41,
-    "Good": 41,
-    "Okay": 13,
-    "Poor": 5,
-    "Terrible": 0
+    "excellent": 41,
+    "good": 41,
+    "okay": 13,
+    "poor": 5,
+    "terrible": 0
   },
   {
     "Day": 26,
-    "Excellent": 43,
-    "Good": 41,
-    "Okay": 12,
-    "Poor": 4,
-    "Terrible": 0
+    "excellent": 43,
+    "good": 41,
+    "okay": 12,
+    "poor": 4,
+    "terrible": 0
   },
   {
     "Day": 27,
-    "Excellent": 45,
-    "Good": 41,
-    "Okay": 10,
-    "Poor": 4,
-    "Terrible": 0
+    "excellent": 45,
+    "good": 41,
+    "okay": 10,
+    "poor": 4,
+    "terrible": 0
   },
   {
     "Day": 28,
-    "Excellent": 45,
-    "Good": 43,
-    "Okay": 10,
-    "Poor": 3,
-    "Terrible": 0
+    "excellent": 45,
+    "good": 43,
+    "okay": 10,
+    "poor": 3,
+    "terrible": 0
   },
   {
     "Day": 29,
-    "Excellent": 44,
-    "Good": 43,
-    "Okay": 10,
-    "Poor": 3,
-    "Terrible": 0
+    "excellent": 44,
+    "good": 43,
+    "okay": 10,
+    "poor": 3,
+    "terrible": 0
   },
   {
     "Day": 30,
-    "Excellent": 45,
-    "Good": 44,
-    "Okay": 9,
-    "Poor": 2,
-    "Terrible": 0
+    "excellent": 45,
+    "good": 44,
+    "okay": 9,
+    "poor": 2,
+    "terrible": 0
   }
 ];
